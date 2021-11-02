d4c3f53f6f63eb3f8851cc4bb78c0cdc51a5ba45
silverback23 <cahid23@hotmail.com>
2021-11-02 13:29:24 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Metotlar
+{ class Program
+ {
+static void Main(string[] args)
+ {
+
+    //erişim belirteci geris_dönüş_tipi metot_adi(paremetre listesi/argüman)
+    //{
+         ///komutlar
+         //return;
+    //}
+    
+    int a=2;
+    int b=3;
+    Console.Write(a+b);
+
+    int sonuc=Topla(a+b);
+    Console.Write(sonuc);
+
+    Metotlar ornek =new Metotlar();
+    ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            
+ }
+
+ static int Topla(int deger1, int deger2)
+ {
+     return  (deger1+deger2);
+ }
+ 
+ }
+
+ class Metotlar
+ {
+     public void EkranaYazdir(string veri)
+     {
+         Console.Write(veri);
+     }
+ }
+
+}
